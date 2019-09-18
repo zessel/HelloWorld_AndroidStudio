4dcc6877a2cb2618ba56f48deca154d7f320c02f
Zach Essel <zachary.essel@gmail.com>
2019-09-17 23:03:56 -0400

    Add button that changes TextView

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,28 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.my_button);
+        View.OnClickListener clicky = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView texty = findViewById(R.id.textView);
+                texty.setText("You have clicked the button");
+            }
+        };
+        button.setOnClickListener(clicky);
     }
+
+
 }
